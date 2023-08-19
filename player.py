2d8f36d9879da452f8833ed92d7cb8efef8431f1
Larjon <kowallllpl@gmail.com>
2023-08-19 17:30:51 +0200

    first commit

---
+++ player.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+class Player:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
---
+++ voxel_render.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+
+class VoxelRender:
+    def __init__(self, app):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
